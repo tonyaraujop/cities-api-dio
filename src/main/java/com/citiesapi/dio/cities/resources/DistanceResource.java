5c917fe9b3581f9180868fe9cb0cf1e035a90d34
Antônio Paulino <tonnyarauujo@gmail.com>
2021-11-25 16:28:12 -0300

    inclusão do response entity

--- src/main/java/com/citiesapi/dio/cities/resources/DistanceResource.java
+++ src/main/java/com/citiesapi/dio/cities/resources/DistanceResource.java
@@ -2,6 +2,7 @@ package com.citiesapi.dio.cities.resources;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,14 +22,14 @@ public class DistanceResource {
 	}
 
 	@GetMapping("/in-miles")
-	public Double byPoints(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
+	public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
 		log.info("byPoints");
-		return service.distanceByPointsInMiles(city1, city2);
+		return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
 	}
 
 	@GetMapping("/in-meters")
-	public Double byCube(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
+	public ResponseEntity byCube(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
 		log.info("byCube");
-		return service.distanceByCubeInMeters(city1, city2);
+		return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
 	}
 }
