24551f102c337b895b40b28ed171e989e0f07dc7
Antônio Paulino <tonnyarauujo@gmail.com>
2021-11-25 16:15:12 -0300

    Calculo de distancia

--- src/main/java/com/citiesapi/dio/cities/repositories/CityRepository.java
+++ src/main/java/com/citiesapi/dio/cities/repositories/CityRepository.java
@@ -1,6 +1,7 @@
 package com.citiesapi.dio.cities.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.citiesapi.dio.cities.entity.City;
@@ -8,4 +9,9 @@ import com.citiesapi.dio.cities.entity.City;
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
+	@Query(value = "SELECT ((SELECT lat_lon FROM cidade WHERE id=?1) <@> (SELECT lat_lon FROM cidade WHERE id=?2)) as distance", nativeQuery = true)
+	Double distanceByPoints(final Long cityId1, final Long cityId2);
+
+	@Query(value = "SELECT earth_distance(ll_to_earth(?1,?2), ll_to_earth(?3,?4)) as distance", nativeQuery = true)
+	Double distanceByCube(final Double lat1, final Double lon1, final Double lat2, final Double lon2);
 }
