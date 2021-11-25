532ff63618ae15615e24100b5bc1aa8bb1620546
Antônio Paulino <tonnyarauujo@gmail.com>
2021-11-25 19:07:08 -0300

    Add PathVariable

--- src/main/java/com/citiesapi/dio/cities/resources/DistanceResource.java
+++ src/main/java/com/citiesapi/dio/cities/resources/DistanceResource.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,14 +22,14 @@ public class DistanceResource {
 		this.service = service;
 	}
 
-	@GetMapping("/in-miles")
-	public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
+	@GetMapping("/in-miles/from/{from}/to/{to}")
+	public ResponseEntity byPoints(@PathVariable(name = "from") final Long city1, @PathVariable(name = "to") final Long city2) {
 		log.info("byPoints");
 		return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
 	}
 
-	@GetMapping("/in-meters")
-	public ResponseEntity byCube(@RequestParam(name = "from") final Long city1, @RequestParam(name = "to") final Long city2) {
+	@GetMapping("/in-meters/from/{from}/to/{to}")
+	public ResponseEntity byCube(@PathVariable(name = "from") final Long city1, @PathVariable(name = "to") final Long city2) {
 		log.info("byCube");
 		return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
 	}
